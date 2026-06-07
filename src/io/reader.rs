3e86f990698ef1d9619847dbbe16b3e7c561615f
Yk4m4 <maxtop1924@yandex.ru>
2026-06-07 16:04:01 +0300

    Complete localization files search and read logic for mods

--- src/io/reader.rs
+++ src/io/reader.rs
@@ -1,41 +1,83 @@
+//! # Note
+//! FIXME: The main functionality is fully implemented and operational, 
+//! but the code structure requires refactoring for better idiomatic Rust
 
-use std::path::{Path, PathBuf};
-use std::fs::{read_to_string, File};
-use std::io;
+use std::path::{Path};
+use std::fs::{self, File};
+use std::io::{self, BufRead, BufReader};
 use zip::ZipArchive;
+use zip::result::ZipError;
 
 use crate::io::check_avaibale_files::TypeRead;
 
-pub fn read_data(core_path: &Path, added_path: &Path, type_read_flag: TypeRead) -> io::Result<Vec<String>>{
 
-    match type_read_flag{
+pub enum ReadError{
+    InvalidFile,
+    WithoutTranslatebleString,
+    Io(io::Error),
+    ZipArchive(ZipError)
+}
 
-        
-        TypeRead::FTBquest =>{
-
-            //add validate path and sepatet read logic in self fn
-
-            let mut out = Vec::new();
-
-
-            //TODO: add safe join
-            let full_path = core_path.join(added_path);
-
-            let readed_data = read_to_string(full_path)?;
-
-            for line in readed_data.lines(){
-                out.push(line.to_string());
-            }
-
-            Ok(out)
-        },
-
-        //placeholder
-        _ => todo!("Implement reading for mod variant"),
+impl From<io::Error> for ReadError{
+    fn from(value: io::Error) -> Self {
+        ReadError::Io(value)
     }
 }
 
+impl From<ZipError> for ReadError{
+    fn from(value: ZipError) -> Self {
+        ReadError::ZipArchive(value)
+    }
+}
 
+pub fn read_data( (path,flag) : (&Path,TypeRead) ) -> Result<Vec<String>,ReadError>{
+
+    match flag{
+        TypeRead::FTBquest => read_quest(path),
+        TypeRead::Modification => read_mod(path),
+    }
+}
+
+fn read_quest(path: &Path) -> Result<Vec<String>,ReadError>{
+
+   if !validate_path(path, TypeFile::File, ExtensionFile::Snbt) {
+        return Err(ReadError::InvalidFile);
+    }
+
+    let readed_data = fs::read_to_string(path)?;
+    
+    let out_data= readed_data.lines().map(|s| s.to_string()).collect();
+
+    Ok(out_data)
+}
+
+fn read_mod(path: &Path) -> Result<Vec<String>,ReadError>{
+
+    if !validate_path(path, TypeFile::File, ExtensionFile::Jar){
+        return Err(ReadError::InvalidFile);
+    };
+
+    let file =  File::open(path)?;
+    let mut archive = ZipArchive::new(file)?;
+
+    for i in 0..archive.len(){
+
+        if let Ok(file_name) = archive.by_index(i){
+            let name_path = file_name.name();
+            
+            if name_path.ends_with("lang/en_us.json"){
+                let mut out: Vec<String> = Vec::new();
+                let reader = BufReader::new(file_name);
+                for i in reader.lines(){
+                    out.push(i?);
+                }
+                return Ok(out);
+            }
+        }
+    }
+
+    Err(ReadError::WithoutTranslatebleString)
+}
 
 enum TypeFile {
     File,
